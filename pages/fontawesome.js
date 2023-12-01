e3cf090e144f3f5009688b42ad72fa004e24b09c
Andri <a8461565@gmail.com>
2023-12-01 22:34:31 +0700

    first commit

--- pages/fontawesome.js
+++ pages/fontawesome.js
@@ -1,6 +1,6 @@
-// fontawesome.js
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faIcon1, faIcon2, /* ... */ } from '@fortawesome/free-solid-svg-icons';
-import { fabIcon1, fabIcon2, /* ... */ } from '@fortawesome/free-brands-svg-icons';
+// // fontawesome.js
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { faIcon1, faIcon2, /* ... */ } from '@fortawesome/free-solid-svg-icons';
+// import { fabIcon1, fabIcon2, /* ... */ } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faIcon1, faIcon2, /* ... */ fabIcon1, fabIcon2, /* ... */);
+// library.add(faIcon1, faIcon2, /* ... */ fabIcon1, fabIcon2, /* ... */);
