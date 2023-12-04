d7b88ae9c19ca1efdca2cc7fb4ddf09f29e0c2bb
Andri <a8461565@gmail.com>
2023-12-04 12:09:35 +0700

    Pesan komit yang benar

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="icon" href="https://www.logolynx.com/images/logolynx/df/dfbcb9d6d8d62765804af677d331eded.png" />
+        </Head>
         <body>
           <Main />
           <NextScript />
