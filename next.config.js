5c4f17360b5c709909abbc5c39fb71fcf36d600e
Andri <a8461565@gmail.com>
2024-01-03 20:27:22 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,30 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+// module.exports = {
+//   images: {
+//     domains: ["flowbite.s3.amazonaws.com", 'andridev.andrianto25.repl.co'],
+//   },
+// };
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Tambahkan konfigurasi untuk modul-modul yang perlu di-transpile di sisi klien
+      config.resolve.fallback = {
+        fs: false, // Hilangkan dukungan fs di sisi klien
+        net: false, // Hilangkan dukungan net di sisi klien
+      };
+    }
+    return config;
+  },
 };
 module.exports = {
+  ...nextConfig,
   images: {
-    domains: ["flowbite.s3.amazonaws.com", 'andridev.andrianto25.repl.co'],
+    domains: ["flowbite.s3.amazonaws.com", "andridev.andrianto25.repl.co"],
   },
 };
